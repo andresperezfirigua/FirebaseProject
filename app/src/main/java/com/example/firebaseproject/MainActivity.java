2e2a19a1a69db7a353cae7fae2ee7c9e1f06215b
Yesika-Vanegas <yesika.vanegas13@gmail.com>
2020-03-25 20:57:54 -0500

    agregar-los-7-campos

--- app/src/main/java/com/example/firebaseproject/MainActivity.java
+++ app/src/main/java/com/example/firebaseproject/MainActivity.java
@@ -12,6 +12,10 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private EditText miNombreCompleto;
     private EditText miApellido;
+    private EditText miEdad;
+    private EditText miCelular;
+    private EditText miCorreo;
+    private EditText miCedula;
     private EditText miUniversidad;
     private Button miBotonMostrar;
     private Button miBotonEnviar;
@@ -22,17 +26,26 @@ public class MainActivity extends AppCompatActivity {
 
         miNombreCompleto = findViewById(R.id.Nombre);
         miApellido = findViewById(R.id.Apellido);
+        miEdad = findViewById(R.id.Edad);
+        miCelular = findViewById(R.id.Celular);
+        miCorreo = findViewById(R.id.Correo);
+        miCedula = findViewById(R.id.Cedula);
         miUniversidad = findViewById(R.id.Universidad);
         miBotonMostrar = findViewById(R.id.Mostrar);
         miBotonEnviar = findViewById(R.id.Enviar);
+
 
         miBotonMostrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String strNombreCompleto = miNombreCompleto.getText().toString();
                 String strApellido = miApellido.getText().toString();
+                String strEdad = miEdad.getText().toString();
+                String strCelular = miCelular.getText().toString();
+                String strCorreo = miCorreo.getText().toString();
+                String strCedula = miCedula.getText().toString();
                 String strUniversidad = miUniversidad.getText().toString();
-                Toast miToast = Toast.makeText(getApplicationContext(),strNombreCompleto+" "+strApellido+" "+strUniversidad,Toast.LENGTH_LONG);
+                Toast miToast = Toast.makeText(getApplicationContext(),strNombreCompleto+" "+strApellido+" "+strEdad+""+strCelular+""+strCorreo+""+strCedula+""+strUniversidad,Toast.LENGTH_LONG);
 
                 miToast.show();
                 /*Estudiante estudiante1 = new Estudiante();
@@ -48,9 +61,17 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), NextActivity.class);
                 String strNombreCompleto = miNombreCompleto.getText().toString();
                 String strApellido = miApellido.getText().toString();
+                String strEdad = miEdad.getText().toString();
+                String strCelular = miCelular.getText().toString();
+                String strCorreo = miCorreo.getText().toString();
+                String strCedula = miCedula.getText().toString();
                 String strUniversidad = miUniversidad.getText().toString();
                 intent.putExtra("clave_nombre",strNombreCompleto);
                 intent.putExtra("clave_apellido", strApellido);
+                intent.putExtra("clave_edad", strEdad);
+                intent.putExtra("clave_celular", strCelular);
+                intent.putExtra("clave_correo", strCorreo);
+                intent.putExtra("clave_cedula", strCedula);
                 intent.putExtra("clave_universidad", strUniversidad);
                 startActivity(intent);
             }
--- app/src/main/java/com/example/firebaseproject/NextActivity.java
+++ app/src/main/java/com/example/firebaseproject/NextActivity.java
@@ -10,6 +10,10 @@ public class NextActivity extends AppCompatActivity {
 
     private TextView Nombre;
     private TextView Apellido;
+    private TextView Edad;
+    private TextView Celular;
+    private TextView Correo;
+    private TextView Cedula;
     private TextView Universidad;
 
     @Override
@@ -19,15 +23,27 @@ public class NextActivity extends AppCompatActivity {
 
         Nombre = findViewById(R.id.textViewNombre);
         Apellido = findViewById(R.id.textViewApellido);
+        Edad = findViewById(R.id.textViewEdad);
+        Celular = findViewById(R.id.textViewCelular);
+        Correo = findViewById(R.id.textViewCorreo);
+        Cedula = findViewById(R.id.textViewCedula);
         Universidad = findViewById(R.id.textViewUniversidad);
 
         Intent intent = getIntent();
         String nombre = intent.getStringExtra("clave_nombre");
         String apellido = intent.getStringExtra("clave_apellido");
+        String edad = intent.getStringExtra("clave_edad");
+        String celular = intent.getStringExtra("clave_celular");
+        String correo = intent.getStringExtra("clave_correo");
+        String cedula = intent.getStringExtra("clave_cedula");
         String universidad = intent.getStringExtra("clave_universidad");
 
         Nombre.setText(nombre);
         Apellido.setText(apellido);
+        Edad.setText(edad);
+        Celular.setText(celular);
+        Correo.setText(correo);
+        Cedula.setText(cedula);
         Universidad.setText(universidad);
     }
 }
